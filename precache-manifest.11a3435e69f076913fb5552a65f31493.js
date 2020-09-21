e128f791fa824b58645ee769dfa375b5da217b77
dia-bhavsar <diabhavsar16@gmail.com>
2020-09-21 23:27:21 +0530

    Updates

---
+++ precache-manifest.11a3435e69f076913fb5552a65f31493.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24576d2fdac4878b6c1b142903aa7bd8",
+    "url": "/Facebook-clone/index.html"
+  },
+  {
+    "revision": "8b8e909fcc73447c63b3",
+    "url": "/Facebook-clone/static/css/main.93830545.chunk.css"
+  },
+  {
+    "revision": "4e56e4c89980727738fe",
+    "url": "/Facebook-clone/static/js/2.0c66a02b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Facebook-clone/static/js/2.0c66a02b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b8e909fcc73447c63b3",
+    "url": "/Facebook-clone/static/js/main.c7757853.chunk.js"
+  },
+  {
+    "revision": "564c40998e9bc23805f2",
+    "url": "/Facebook-clone/static/js/runtime-main.836a199b.js"
+  }
+]);
